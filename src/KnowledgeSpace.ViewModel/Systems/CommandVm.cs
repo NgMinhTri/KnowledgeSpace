b6f22eca936c04bf75159932464b9730893a38ee
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2021-08-11 21:13:52 +0700

    API Quản lý action cho function

---
+++ src/KnowledgeSpace.ViewModel/Systems/CommandVm.cs
@@ -0,0 +1,8 @@
+﻿namespace KnowledgeSpace.ViewModel.Systems
+{
+    public class CommandVm
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/KnowledgeSpace.ViewModel/Systems/PostCommandToFunctionVm.cs
@@ -0,0 +1,8 @@
+﻿namespace KnowledgeSpace.ViewModel.Systems
+{
+    public class PostCommandToFunctionVm
+    {
+        public string CommandId { get; set; }
+        public string FunctionId { get; set; }
+    }
+}
