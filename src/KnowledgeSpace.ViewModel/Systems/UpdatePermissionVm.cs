2d843c64334e2dce45f085d16b70c80cf797a709
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2021-08-12 20:33:47 +0700

    Xây dựng API quản lý permission

---
+++ src/KnowledgeSpace.ViewModel/Systems/UpdatePermissionVm.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace KnowledgeSpace.ViewModel.Systems
+{
+    public class UpdatePermissionVm
+    {
+        public List<PermissionVm> Permissions { get; set; } = new List<PermissionVm>();
+    }
+}
