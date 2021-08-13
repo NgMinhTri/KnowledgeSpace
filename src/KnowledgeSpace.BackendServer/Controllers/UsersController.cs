32a237bcf358d38c8dbabe6e9f4f3409aa440012
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2021-08-13 20:10:33 +0700

    Xây dựng API Quản lý bài đăng

--- src/KnowledgeSpace.BackendServer/Controllers/UsersController.cs
+++ src/KnowledgeSpace.BackendServer/Controllers/UsersController.cs
@@ -141,6 +141,22 @@ namespace KnowledgeSpace.BackendServer.Controllers
             return BadRequest(result.Errors);
         }
 
+        [HttpPut("{id}/change-password")]
+        public async Task<IActionResult> PutPasswordUser(string id, [FromBody] PutPasswordUserVm request)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            var result = await _userManager.ChangePasswordAsync(user, request.CurrentPassword, request.NewPassword);
+
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+            return BadRequest(result.Errors);
+        }
+
         [HttpDelete("id")]
         public async Task<IActionResult> DeleteUser(string id)
         {
---
+++ src/KnowledgeSpace.ViewModel/Contents/KnowledgeBaseQuickVm.cs
@@ -0,0 +1,18 @@
+﻿namespace KnowledgeSpace.ViewModel.Contents
+{
+    public class KnowledgeBaseQuickVm
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Title { get; set; }
+
+        public string SeoAlias { get; set; }
+
+        public string Description { get; set; }
+    }
+
+}
