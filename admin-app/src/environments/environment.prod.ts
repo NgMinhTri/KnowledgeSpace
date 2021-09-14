104bd197c9e2b7a0376fbdf5d4b390472630864a
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2021-09-14 20:02:12 +0700

    Ghép giao diện cho Web Portal

--- admin-app/src/environments/environment.prod.ts
+++ admin-app/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://kbapi.tedu.com.vn'
+  apiUrl: 'https://kbapi.tedu.com.vn',
+  authorityUrl: 'https://localhost:5000',
+  clientId: 'angular_admin',
+  adminUrl: 'http://localhost:4200'  
 };
--- src/KnowledgeSpace.WebPortal/Controllers/HomeController.cs
+++ src/KnowledgeSpace.WebPortal/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using KnowledgeSpace.WebPortal.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KnowledgeSpace.WebPortal.Controllers
 {
@@ -23,10 +19,6 @@ namespace KnowledgeSpace.WebPortal.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
