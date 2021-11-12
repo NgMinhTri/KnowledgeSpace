1a2a0647bc862edd60057ea85f0867f7755c03d7
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2021-11-12 21:33:38 +0700

    UnitTest cho RoleController

--- src/KnowledgeSpace.BackendServer/Controllers/RolesController.cs
+++ src/KnowledgeSpace.BackendServer/Controllers/RolesController.cs
@@ -42,19 +42,19 @@ namespace KnowledgeSpace.BackendServer.Controllers
         [HttpGet]
         public async Task<IActionResult> GetRoles()
         {
-            var role = await _roleManager.Roles
-            .Select(r => new RoleVm()
+            var roles = await _roleManager.Roles.ToListAsync();
+            var rolevms = roles.Select(r => new RoleVm()
             {
                 Id = r.Id,
                 Name = r.Name
-            })
-            .ToListAsync();
-            return Ok(role);
+            });
+            
+            return Ok(rolevms);
         }
 
         //URL: GET: http://localhost:5001/api/roles/?filter={filter}&pageIndex=1&pageSize=10
-        [HttpGet("filter")]
-        public async Task<IActionResult> GetRoles(string filter, int pageIndex, int pageSize)
+        [HttpGet]
+        public async Task<IActionResult> GetRolesPaging(string filter, int pageIndex, int pageSize)
         {
             var query = _roleManager.Roles;
             if (!string.IsNullOrEmpty(filter))
